e34881311c435ba0240856b6a27c058fcd7368f5
devanjalisharma <devanjali003@gmail.com>
2023-02-13 09:43:55 +0530

    .

--- computational_lab3.java
+++ computational_lab3.java
@@ -4,7 +4,7 @@
  public static void main(String args[])
  {
     Scanner in =new Scanner (System.in);
-    int i;
+    int i,p=1;
     double x,iar,a,g,f;
     System.out.println("Enter initial approximated root");
     iar=in.nextDouble();
@@ -16,7 +16,7 @@
         f=(x*x*x)-x-1;
         g=3*x*x-1;
         a=x-(f/g);
-        System.out.println("x  "+a);
+        System.out.println("x"+ p++ +"  "+a);
         x=a;
         a=0;
     }
