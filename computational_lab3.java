aa254236703b2b45306e2d7ece2e5b2a3f40fd07
devanjalisharma <devanjali003@gmail.com>
2023-02-13 09:37:31 +0530

    .

---
+++ computational_lab3.java
@@ -0,0 +1,24 @@
+ import java.util.*;
+ class computational_lab3
+{
+ public static void main(String args[])
+ {
+    Scanner in =new Scanner (System.in);
+    int i;
+    double x,iar,a,g,f;
+    System.out.println("Enter initial approximated root");
+    iar=in.nextDouble();
+    x=iar;
+    System.out.println("Enter number of iterations");
+    i=in.nextInt();
+    for(int j=0;j<i;j++)
+    {
+        f=(x*x*x)-x-1;
+        g=3*x*x-1;
+        a=x-(f/g);
+        System.out.println("x  "+a);
+        x=a;
+        a=0;
+    }
+ }   
+}
---
+++ water_container.java
@@ -0,0 +1,16 @@
+import java.util.*;
+ class water_container
+{
+    public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+        int no;
+        System.out.println("Enter number of walls");
+        no=in.nextInt();
+        int height[]=new int[no];
+        System.out.println("Enter heights of walls");
+        for(int i=0;i<no;i++)
+        height[i]=in.nextInt();
+        
+    }    
+}
